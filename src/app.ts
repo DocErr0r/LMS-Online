b786e1a40932756f134841ebc893972ac6a6db05
Nitin <118925609+Nitin-NK-01@users.noreply.github.com>
2025-06-11 19:21:38 +0530

    create tesing route and ts config

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,19 @@
+import cookieParser from 'cookie-parser';
 import express, { NextFunction, Request, Response } from 'express';
 export const app = express();
+require('dotenv').config();
+import cors from 'cors';
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cookieParser());
+
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    }),
+);
 
 app.get('/test', (req: Request, res: Response) => {
     res.status(200).json({
@@ -8,7 +22,7 @@ app.get('/test', (req: Request, res: Response) => {
     });
 });
 
-app.get('*', (req: Request, res: Response, next: NextFunction) => {
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
     const err = new Error(`Route not found - ${req.originalUrl}`) as any;
     err.statusCode = 404;
     next(err);
